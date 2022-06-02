48e33c16c49a18863ff209cbaf58f00ebae80a59
Lopulalan Randy Yonathan <randylopulalan07@gmail.com>
2022-06-02 17:31:46 +0800

    update .delete

--- app/product-v2/controllerV2.js
+++ app/product-v2/controllerV2.js
@@ -1,21 +1,110 @@
-const Product = require('./model')
-const path = require('path')
-const fs = require('fs')
+const Product = require("./model");
+const path = require("path");
+const fs = require("fs");
 
-const store = async (req,res) => {
-    const {users_id, name, price, stock, status, image_url} = req.body
-    const image = req.file
-    if (image) {
-        const target = path.join(__dirname, "../../uploads", image.originalname);
-        fs.renameSync(image.path, target);
-    }
-    try{
-        await Product.sync();
-        const result = await Product.create({users_id, name, price, stock, status, image_url: `https//localhost:3000/public/${image.originalname}`})
-        res.send(result)
-    } catch (err) {
-        res.send(err)
-    }
-}
+// GET index
+const index = async (req, res) => {
+  const { search } = req.query;
+  let result;
+  if (!search) {
+    result = await Product.findAll();
+  } else {
+    result = await Product.findAll({
+      where: { name: { [Op.substring]: [`${search}%`] } },
+    });
+  }
+  try {
+    res.send(result);
+  } catch (err) {
+    res.send(err.message);
+  }
+};
 
-module.exports = {store}
+// GET view id
+const view = async (req, res) => {
+  try {
+    await Product.sync();
+    const result = await Product.findAll({ where: { id: req.params.id } });
+    res.send(result);
+  } catch (err) {
+    res.send(err.message);
+  }
+};
+
+// POST
+const store = async (req, res) => {
+  const { users_id, name, price, stock, status, image_url } = req.body;
+  const image = req.file;
+  if (image) {
+    const target = path.join(__dirname, "../../uploads", image.originalname);
+    fs.renameSync(image.path, target);
+  }
+  try {
+    await Product.sync();
+    const result = await Product.create({
+      users_id,
+      name,
+      price,
+      stock,
+      status,
+      image_url: `https//localhost:3000/public/${image.originalname}`,
+    });
+    res.send(result);
+  } catch (err) {
+    res.send(err);
+  }
+};
+
+// PUT
+const update = async (req, res) => {
+  const { users_id, name, price, stock, status, image_url } = req.body;
+  const image = req.file;
+  let result;
+  if (image) {
+    const target = path.join(__dirname, "../../uploads", image.originalname);
+    fs.renameSync(image.path, target);
+    result = await Product.update(
+      {
+        users_id,
+        name,
+        price,
+        stock,
+        status,
+        image_url: `https//localhost:3000/public/${image.originalname}`,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+  } else {
+    result = await Product.update(
+      { users_id, name, price, stock, status },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+  }
+  try {
+    await Product.sync();
+    res.send(result);
+  } catch (err) {
+    res.send(err);
+  }
+};
+
+// DELETE
+const deleteData = async (req, res) => {
+  try {
+    await Product.sync();
+    await Product.destroy({ where: { id: req.params.id } });
+    res.send(`Success delete data product id: ${req.params.id}`);
+  } catch (err) {
+    res.send(err.message);
+  }
+};
+
+module.exports = { index, view, store, update, deleteData };
--- app/product-v2/routes.js
+++ app/product-v2/routes.js
@@ -3,6 +3,11 @@ const multer = require('multer')
 const upload = multer({dest: "uploads"})
 const productControllerV2 = require('./controllerV2')
 
-router.post('/product/', upload.single("image"),  productControllerV2.store)
+router.get("/product", productControllerV2.index);
+router.get("/product/:id", productControllerV2.view);
+router.post("/product/", upload.single("image"), productControllerV2.store);
+router.put("/product/:id", upload.single("image"), productControllerV2.update);
+router.delete("/product/:id",upload.single("image"),productControllerV2.deleteData);
+
 
 module.exports = router;
